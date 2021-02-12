02d0fd6db27a0ca49993615b89c0252b3a570531
guillaume <guillaume@lepointdevente.com>
2021-02-12 01:43:05 -0500

    init vueApp and vue-chartjs

---
+++ dashboard/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Gaul logo" src="./assets/gaul.webp" style="width: 250px;">
+    <h1>Ground Station Dashboard</h1>
+    <div class="grid-container">
+      <div class="grid-item"><PlotChart id="plot-chart"></PlotChart></div>
+      <div class="grid-item"><PlotChart id="plot-chart"></PlotChart></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlotChart from './components/PlotChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PlotChart,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#plot-chart {
+  width: 20%;
+  height: 20%;
+}
+.grid-container {
+  display: grid;
+  grid-gap: 50px 50px;
+}
+</style>
---
+++ dashboard/src/components/PlotChart.vue
@@ -0,0 +1,46 @@
+<template>
+	<chart :chart-data="datacollection" :width="200" :height="200"></chart>
+</template>
+
+<script>
+import Chart from "./../chart-model.js";
+export default {
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ["January","February","March","April","May","June","July","August","September"],
+        // labels: [this.rand(), this.rand()],
+        datasets: [
+          {
+            label: "Data 1",
+            backgroundColor: "red",
+            data: [this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand()]
+          },
+          {
+            label: "Data 2",
+            backgroundColor: "gold",
+            data: [this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand(), this.rand()]
+          }
+        ]
+      };
+    },
+    rand() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
